04525ce8797ff3cb4daf602f1bfcf4aa3536f73b
AbooSalh <a.m0507979@gmail.com>
2024-05-09 08:42:58 +0300

    width scroll fix

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -47,7 +47,7 @@ const PortfolioPage = () => {
           </motion.div>
         </div>
       </div>
-      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+      <div className="w-full h-screen flex flex-col gap-16 items-center justify-center text-center">
         <h1 className="text-8xl">Do you have a project?</h1>
         <div className="relative">
           <motion.svg
